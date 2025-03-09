6607c91b65e5e580baa8faad44f000fb5abbaa59
TheRealGo <v.greenultrabiso@gmail.com>
2025-03-09 21:28:25 +0900

    first commit

---
+++ openai-ocr-multiformat-rs/src/main.rs
@@ -0,0 +1,16 @@
+use pdf2image::{PDF2ImageError, RenderOptionsBuilder, PDF};
+
+fn main() -> Result<(), PDF2ImageError> {
+    let pdf = PDF::from_file("./examples/Pythonチュートリアル.pdf").unwrap();
+    let pages = pdf.render(
+        pdf2image::Pages::Range(1..=8),
+        RenderOptionsBuilder::default().pdftocairo(true).build()?,
+    )?;
+
+    std::fs::create_dir("./examples/out").unwrap();
+    for (i, page) in pages.iter().enumerate() {
+        page.save_with_format(format!("./examples/out/{}.jpg", i + 1), image::ImageFormat::Jpeg)?;
+    }
+
+    Ok(())
+}
